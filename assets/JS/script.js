fb5a8f5a118f840f97997d1171653cb0080c1e2c
aedelfrid <almck51@gmail.com>
2023-05-25 01:01:52 -0300

    changed css styling, added fonts

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,31 +1,36 @@
-var beginButton = document.querySelector("#begin");
-var menu = document.querySelector("#menu")
-var header = document.querySelector("header")
+var beginButton = document.querySelector('#begin');
+var menu = document.querySelector('#menu')
+var header = document.querySelector('header')
+var buttons = document.querySelector('#buttons')
 
 
-var timeLeft = 60;
+var timeLeft = 30;
 
-beginButton.addEventListener("click",begin);
+beginButton.addEventListener('click',begin);
 
 
 function begin() {
-    var timer = document.createElement("h2");
+    var timer = document.createElement('h2');
     header.appendChild(timer);
-    timer.setAttribute
-    timer.textContent = ("Time Left: " + timeLeft);
+    timer.setAttribute('id', 'timer')
+    timer.textContent = ('Time Left: ' + timeLeft);
+
+    beginButton.setAttribute('style','visibility:hidden')
 
     var timeInterval = setInterval(function() {
         timeLeft--;
-        timer.textContent = ("Time Left: " + timeLeft);
+        timer.textContent = ('Time Left: ' + timeLeft);
 
         if (timeLeft == 0) {
             clearInterval(timeInterval)
-            timer.textContent = "";
+            timer.textContent = 'Out of Time!';
             gameOver()
         }
     }, 1000);
 }
 
 function gameOver() {
-    timeLeft = 60;
+    timeLeft = 30;
+    beginButton.setAttribute('style','visibility:visible')
+
 }
